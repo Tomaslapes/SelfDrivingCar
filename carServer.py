4e9e32281149cf88f096973ebca0c5c9c4303914
Tomas Lapes-JetsonNano <tom@x-com.cz>
2021-02-03 13:18:53 +0100

    Server - Camera works

--- carServer.py
+++ carServer.py
@@ -2,6 +2,8 @@ import socket
 import cv2
 import pickle
 import threading
+from time import sleep
+import numpy as np
 
 # Server Address
 IP = "192.168.15.15"  # socket.gethostbyname(socket.getfqdn())
@@ -11,7 +13,7 @@ ADDR = (IP, PORT)
 # Communication settings
 HEADER = 10
 # Camera setup
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0, cv2.CAP_V4L)
 
 # Ques
 COMMAND_LIST = []
@@ -27,21 +29,29 @@ print(f"[STATUS] Server is listening to incoming connections!")
 SERVER_ONLINE = True
 
 
-def send_camera_feed():
-    while True:
-        ret, frame = cam.read()
-        encoded_image = pickle.dump(frame)
-        padded_msg = bytes(
-            f"{len(encoded_image):<{HEADER}}", "utf-8") + encoded_image
-        print(len(padded_msg))
-        #client_socket.send(padded_msg)
+def send_camera_feed(cam,HEADER,client_socket):
+    print("Send camera data")
+    #while True:
+    ret, frame = cam.read()
+    if type(frame) == type(None):
+        frame = None#np.zeros((10,10))
+    img = cv2.resize(frame,(50,50))
+
+    encoded_image = pickle.dumps(img)
+    prefix =bytes(f"{len(encoded_image):<{HEADER}}", "utf-8")
+    padded_msg = prefix + encoded_image
+    print("Image size: ", len(padded_msg))
+    client_socket.send(padded_msg)
+    print("send") 
+        
 
 
 while SERVER_ONLINE:
     client_socket, client_addr = server_socket.accept()
     print(f"[INFO] New connection from address {client_addr} established!")
+    #camera_thread = threading.Thread(target=send_camera_feed,args=[cam,HEADER,client_socket], daemon=False)
+    #camera_thread.start()
     while client_socket:
-        camera_thread = threading.Thread(target=send_camera_feed, daemon=True)
         try:
             message_init = client_socket.recv(HEADER)
             msg_length = int(message_init)
@@ -50,7 +60,10 @@ while SERVER_ONLINE:
             message = message.decode('utf-8')
             print(message)
 
-        except:
+            send_camera_feed(cam,HEADER,client_socket)
+
+        except Exception as e:
+            print(e)
             SERVER_ONLINE = False
             client_socket.shutdown(socket.SHUT_RDWR)
             break
