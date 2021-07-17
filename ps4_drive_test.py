a8c0708aef762e5de995e906b187d545894e0511
Tomaslapes <tom@x-com.cz>
2021-07-17 17:13:31 +0200

    Update ps4_drive_test.py

--- ps4_drive_test.py
+++ ps4_drive_test.py
@@ -1,7 +1,6 @@
 from libs import Car
 from pyPS4Controller.controller import Controller
-
-
+from time import sleep
 
 # Car setup
 CAR = Car.Car((32), (11, 13, 33, True), (31, 29, 33, True))
@@ -18,10 +17,12 @@ class MyController(Controller):
     def on_L3_up(self,value):
         CAR_SPEED = (int(value)/MAX_VALUE)*100
         CAR.update_car(CAR_STEER, int(CAR_SPEED))
+        sleep(0.1)
 
     def on_L3_down(self,value):
         CAR_SPEED = (int(value)/MAX_VALUE)*100
         CAR.update_car(CAR_STEER, int(CAR_SPEED))
+        sleep(0.1)
 
 PS4_CONTROLLER = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 PS4_CONTROLLER.listen(timeout=100)
