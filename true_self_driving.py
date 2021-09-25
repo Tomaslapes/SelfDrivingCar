f23842b7723e188f1ef1942e8084a9025fa74601
Tomaslapes <tom@x-com.cz>
2021-09-25 20:57:57 +0200

    Update true_self_driving.py

--- true_self_driving.py
+++ true_self_driving.py
@@ -1,18 +1,23 @@
+from rich.console import Console
+console = Console()
+
 import torch
+console.log("[green3]Torch loaded")
 from libs import Car
 import cv2
-from rich.console import Console
+console.log("[green3]CV2 loaded")
 
-console = Console()
 
 # Model setup
 model = torch.load("/Models/3_0-07Steer 5E 42BS LR0-0001 gpu.pth")
+console.log("[green3]Model loaded successfully!")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Car setup
 CAR = Car.Car((32), (11, 13, 33, True), (31, 29, 33, True))
 CAR_SPEED = 87
 CAR_STEER = 0.5
+console.log("[green3]CAR initialized successfully!")
 
 # Camera setup
 camera = cv2.VideoCapture(0, cv2.CAP_V4L)
@@ -22,6 +27,7 @@ camera.set(cv2.CAP_PROP_EXPOSURE, -4)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH,320)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
 camera.set(cv2.CAP_PROP_BUFFERSIZE,1)
+console.log("[green3]Camera initialized successfully!")
 
 
 DRIVING = True
@@ -31,6 +37,9 @@ while DRIVING:
     #ret, frame = camera.read()
     ret,frame = camera.retrieve(camera.grab())
     input_img = torch.from_numpy(frame)
+    input_img = input_img.transpose(0,2).transpose(1,2)
+    input_img.unsqueeze_(0)
+    console.log(input_img.shape)
     output = model(input_img)
     console.log(output.shape)
     output = torch.reshape(output,(-1,))
