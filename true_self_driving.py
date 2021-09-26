db9d243bb4ffe435cf59ed54df08276751f5f6e7
Tomaslapes <tom@x-com.cz>
2021-09-26 21:29:16 +0200

    Update true_self_driving.py

--- true_self_driving.py
+++ true_self_driving.py
@@ -20,7 +20,7 @@ with console.status("Started to load the model...",spinner="moon"):
     model = models.resnet18()
     model.fc = nn.Linear(model.fc.in_features,1)
     model.load_state_dict(torch.load("./Models/3_0-07Steer 5E 42BS LR0-0001 gpu_dict.pth"))
-    model.to(DEVICE)
+    # model.to(DEVICE)
     model.eval()
     console.log("[green3]Model loaded successfully!")
 
@@ -43,26 +43,27 @@ console.log("[green3]Camera initialized successfully!")
 
 DRIVING = True
 
-while DRIVING:
-    # ********** IMAGE PROCCESSING ********** #
-    #ret, frame = camera.read()
-    ret,frame = camera.retrieve(camera.grab())
-    console.log("Retrieved a frame")
-    input_img = torch.from_numpy(frame)
-    input_img = input_img.to(DEVICE)
-    input_img = input_img.transpose(0,2).transpose(1,2)
-    input_img.unsqueeze_(0)
-    input_img = Resize((244,244))(input_img)
-    input_img = input_img.float() / 255.0
-    console.log("Image is preped. Shape: ",input_img.shape)
+with torch.no_grad():
+    while DRIVING:
+        # ********** IMAGE PROCCESSING ********** #
+        #ret, frame = camera.read()
+        ret,frame = camera.retrieve(camera.grab())
+        console.log("Retrieved a frame")
+        input_img = torch.from_numpy(frame)
+        # input_img = input_img.to(DEVICE)
+        input_img = input_img.transpose(0,2).transpose(1,2)
+        input_img.unsqueeze_(0)
+        input_img = Resize((244,244))(input_img)
+        input_img = input_img.float() / 255.0
+        console.log("Image is preped. Shape: ",input_img.shape)
 
-    output = model(input_img)
+        output = model(input_img)
 
-    console.log(output.shape)
-    output = torch.reshape(output,(-1,))
-    console.log(output.shape)
-    console.log(output)
+        console.log(output.shape)
+        output = torch.reshape(output,(-1,))
+        console.log(output.shape)
+        console.log(output)
 
-    CAR.update_car(output.item(), CAR_SPEED)
+        CAR.update_car(output.item(), CAR_SPEED)
 
 
